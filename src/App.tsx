d447c11d0bb201627bb3ec8b79d2b375f3657b93
KhristenkoE <ekhristenko0@gmail.com>
2022-08-02 16:51:22 +0400

    add basic components layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import DataContext, { initialContext as initialDataContext } from './contexts/DataContext';
+import TransactionsList from './pages/TransactionsList/TransactionsList';
 
-const App = () => <>Hello World</>;
+const App = () => {
+
+	return (
+		<DataContext.Provider value={initialDataContext}>
+			<TransactionsList />
+		</DataContext.Provider>
+	)
+};
 
 export default App;
---
+++ src/pages/TransactionsList/TransactionsList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
+import ItemsList from '../../components/ItemsList/ItemsList';
+
+
+const TransactionsList = () => {
+	return (
+		<section>
+			<Breadcrumbs />
+			<ItemsList />
+		</section>
+	);
+};
+
+export default TransactionsList;
