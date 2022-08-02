bf59be78ec59e233eeab35161fecc3312db20c85
KhristenkoE <ekhristenko0@gmail.com>
2022-08-02 18:40:09 +0400

    add config for css parsing

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,19 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+			{
+				test: /\.css$/,
+				use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+			}
     ],
   },
 
