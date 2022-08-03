25e2d332e53b13934cfdaad4ca7e5ca75bed384d
KhristenkoE <ekhristenko0@gmail.com>
2022-08-03 20:14:31 +0400

    add 404 page

---
+++ src/pages/_404/_404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const _404 = () => {
+	return (
+		<section>
+			<h1>Requested page Not Found</h1>
+			<a href="/home">Go home</a>
+		</section>
+	)
+};
+
+export default _404;
