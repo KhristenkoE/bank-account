9159357b56a73c315d19b9bb1e5e27ebf82557ff
KhristenkoE <ekhristenko0@gmail.com>
2022-08-03 20:15:23 +0400

    add routing to index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-
+import { BrowserRouter } from 'react-router-dom';
 import './css/index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<BrowserRouter basename='home'>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
+);
