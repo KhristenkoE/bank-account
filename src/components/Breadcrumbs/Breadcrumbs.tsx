8920f955643d8f12e16bf0484d821311ce3b3b6a
KhristenkoE <ekhristenko0@gmail.com>
2022-08-04 15:04:13 +0400

    add breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BreadcrumbsContext } from '../../contexts/BreadcrumbsContext';
+import styles from './Breadcrumbs.module.css';
 
 const Breadcrumbs = () => {
-	return <></>;
+	const { breadcrumbs, setBreadcrumbs } = useContext(BreadcrumbsContext);
+	const navigate = useNavigate();
+
+	const onBreadcrumbClick = (url: string, index: number) => {
+		navigate(url);
+	}
+
+	return (
+		<nav>
+			<ul className={styles.list}>
+				{breadcrumbs.map(({ url, name }, index) => (
+					<li key={index}>
+						<button className={index === breadcrumbs.length - 1 ? styles.breadcrumbActive : ''} onClick={() => onBreadcrumbClick(url, index)}>{name}</button>
+					</li>
+				))}
+			</ul>
+		</nav>
+	);
 };
 
 export default Breadcrumbs;
