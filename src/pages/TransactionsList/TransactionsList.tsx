c1245063c286aa869d53a8529618f21e251e118f
KhristenkoE <ekhristenko0@gmail.com>
2022-08-04 15:43:00 +0400

    add filter logic to TransactionsList

--- src/pages/TransactionsList/TransactionsList.tsx
+++ src/pages/TransactionsList/TransactionsList.tsx
@@ -1,24 +1,47 @@
-import React, { useContext, useEffect, useMemo, useRef } from 'react';
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Filter from '../../components/Filter/Filter';
 import ItemsList from '../../components/ItemsList/ItemsList';
 import DataContext from '../../contexts/DataContext';
 import { FiltersContext } from '../../contexts/FiltersContext';
-import { FilterType } from '../../interfaces/Filter';
+import { FilterName, FilterType } from '../../interfaces/Filter';
 import Transaction from './Transaction/Transaction';
 import styles from './TransactionsList.module.css';
 
 const TransactionsList = () => {
-	const { transactions, filters } = useContext(DataContext);
-	const { setTransactionFilters, transactionFilters, appliedTransactionFilters, setAppliedTransactionFilters } = useContext(FiltersContext);
+	const { transactions, transactionFilters } = useContext(DataContext);
+	const { appliedTransactionFilters, setAppliedTransactionFilters } = useContext(FiltersContext);
 	const debounceTimeoutId = useRef<NodeJS.Timeout>(null);
+	const [page, setPage] = useState(1);
 
-	useEffect(() => {
-		setTransactionFilters(filters);
-	}, []);
+	// TODO: remove filter logic from component
+	const filteredTransactions = useMemo(() => (transactions.filter((transaction) => {
+		return Object.entries(appliedTransactionFilters).every(([name, value]) => {
+			if (name === FilterName.CARD_ACCOUNT) {
+				return transaction.cardAccount === value;
+			}
 
-	const updateFilters = (value: string | { min: number, max: number }, name: string) => {
+			if (name === FilterName.CARD_ID || name === FilterName.CURRENCY) {
+				return transaction[name] === value;
+			}
+
+			if (name === FilterName.AMOUNT) {
+				const filterValue = value as { min: number; max: number };
+				return transaction.amount >= filterValue.min && transaction.amount <= filterValue.max;
+			}
+
+			if (name === FilterName.DATE) {
+				const formatedDate: Date = new Date(value as string);
+				return transaction.transactionDate === `${formatedDate.getDate()}/${formatedDate.getMonth() + 1}/${formatedDate.getFullYear()}`;
+			}
+
+			return true;
+		}
+	)})), [transactions, appliedTransactionFilters]);
+		
+	const updateFilters = (value: string | { min: number, max: number }, name: FilterName) => {
 		const newFilters = { ...appliedTransactionFilters };
+		setPage(1);
 		if (!value || value === 'All') {
 			delete newFilters[name];
 			setAppliedTransactionFilters(newFilters);
@@ -27,7 +50,7 @@ const TransactionsList = () => {
 		setAppliedTransactionFilters({ ...appliedTransactionFilters, [name]: value });
 	}
 
-	const onFilterChange = (value: string | { min: number, max: number }, name: string, type: FilterType, ) => {
+	const onFilterChange = (value: string | { min: number, max: number }, name: FilterName, type: FilterType, ) => {
 		if (type === FilterType.RANGE) {
 			clearTimeout(debounceTimeoutId.current);
 
@@ -39,21 +62,26 @@ const TransactionsList = () => {
 
 		updateFilters(value, name);
 	};
-console.log(appliedTransactionFilters, 'ap')
+
 	return (
 		<section className={styles.section}>
 			<div className={styles.topMenu}>
 				<h1>Transactions</h1>
-				<Filter onChange={onFilterChange} filters={transactionFilters} />
+				<Filter intialAppliedFilters={appliedTransactionFilters} onClear={() => setAppliedTransactionFilters({})} onChange={onFilterChange} filters={transactionFilters} />
 			</div>
 			<ItemsList>
 				<div id='filters-block'></div>
-				{transactions.map((transaction) =>
+				{filteredTransactions.slice((page - 1) * 10, (page - 1) * 10 + 10).map((transaction) =>
 					<Link key={transaction.transactionID} className={styles.transactionLink} to={`${transaction.transactionID}`}>
 						<Transaction transaction={transaction} />
 					</Link>
 				)}
 			</ItemsList>
+			<div className={styles.nav}>
+				{Array.from({ length: Math.ceil(filteredTransactions.length / 10) }, (_, index) =>
+					<button className={`${styles.navButton} ${index + 1 === page ? styles.navButtonActive : ''}`} key={index} onClick={() => setPage(index + 1)}>{index + 1}</button>
+				)}
+			</div>
 		</section>
 	);
 };
