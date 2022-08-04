51e83f2133d22e5525d61e37ff2233037f35ea15
KhristenkoE <ekhristenko0@gmail.com>
2022-08-04 15:07:45 +0400

    make FilterDate controllable

--- src/components/Filter/FilterDate/FilterDate.tsx
+++ src/components/Filter/FilterDate/FilterDate.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styles from './FilterDate.module.css';
 
 interface Props {
 	onChange(value: string): void;
+	initialValue?: string;
 }
 
 const FilterDate = ({
 	onChange,
+	initialValue,
 }: Props) => {
+	const [value, setValue] = React.useState(initialValue);
+
+	useEffect(() => {
+		setValue(initialValue);
+	}, [initialValue]);
+
+	const changeValue = (value: string) => {
+		onChange(value);
+		setValue(value);
+	};
+
 	return (
-		<input onChange={(e) => onChange(e.target.value)} type="date" />
+		<input className={styles.datePicker} value={value} onChange={(e) => changeValue(e.target.value)} type="date" />
 	)
 }
 
