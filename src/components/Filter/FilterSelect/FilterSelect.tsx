816910426ef89ab3355b778aa8ca1219ff3a91b3
KhristenkoE <ekhristenko0@gmail.com>
2022-08-04 15:09:34 +0400

    fix logic and styles of FilterSelect

--- src/components/Filter/FilterSelect/FilterSelect.tsx
+++ src/components/Filter/FilterSelect/FilterSelect.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FilterOption } from '../../../interfaces/Filter';
+import styles from './FilterSelect.module.css';
 
 interface Props {
 	options: FilterOption[];
 	name: string;
+	initialValue?: string;
 	onChange: (value: string) => void;
 }
 
 const FilterSelect = ({
 	options,
 	name,
+	initialValue,
 	onChange,
 }: Props) => {
+	const [selectedOption, setSelectedOption] = React.useState(initialValue);
+
+	useEffect(() => {
+		setSelectedOption(initialValue);
+	}, [initialValue]);
+
+	const changeValue = (value: string) => {
+		onChange(value);
+		setSelectedOption(value);
+	}
+
 	return (
-		<select onChange={(e) => onChange(e.target.value)} name={name} id={name}>
+		<select className={styles.select} value={selectedOption} onChange={(e) => changeValue(e.target.value)} name={name} id={name}>
 			{options.map(({ label, value }) => (
 				<option value={value} key={value}>{label}</option>
 			))}
